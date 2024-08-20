5dded2840d59edbc0cd6d5fa5006850d13ccd643
ChenWei <chen158_7@126.com>
2024-08-20 15:26:50 +0800

    feat: docs& dev test

--- xterio-auth/src/main.ts
+++ xterio-auth/src/main.ts
@@ -1,8 +1,7 @@
-import { log } from 'utils'
 import * as XterioAuth from './index'
 import type { IUserInfo } from './index'
 
-log('initial')
+console.log('initial')
 const redirect_uri = 'http://localhost:3000/'
 const client_id = '4gsmgur6gkp8u9ps8dlco3k7eo'
 //4gsmgur6gkp8u9ps8dlco3k7eo, 4gsmgur6gkp8u9ps8dlco3aaaa
