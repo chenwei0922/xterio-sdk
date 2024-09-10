0ea8a0ff164881a25e30ec8a70b4913d83d3ed24
ChenWei <chen158_7@126.com>
2024-09-10 16:54:07 +0800

    feat: debug deploy page

--- sh/deploy.ts
+++ sh/deploy.ts
@@ -1,14 +1,21 @@
 import chalk from 'chalk'
 import consola from 'consola'
 import { copy, emptyDir, ensureDir } from 'fs-extra'
-import { pathAuth, pathWallet, pathExampleAuthReact, pathExampleAuthReactOutput } from './paths'
+import {
+  pathAuth,
+  pathWallet,
+  pathExampleAuthReact,
+  pathExample,
+  pathExampleOutput,
+  pathExampleOutputAuthReact
+} from './paths'
 import { run } from './run'
-import { dirname, resolve } from 'path'
+import { dirname, resolve, join } from 'path'
 
 async function init() {
   //example-auth-react
   const srcdist = resolve(pathExampleAuthReact, 'dist')
-  const outDist = pathExampleAuthReactOutput
+  const outDist = pathExampleOutputAuthReact
   await emptyDir(srcdist)
   await emptyDir(outDist)
   await run('npm run build', pathExampleAuthReact)
@@ -17,6 +24,9 @@ async function init() {
   //example-auth-vue
 
   //example-wallet-react
+
+  //index.html
+  await copy(join(pathExample, 'index.html'), join(pathExampleOutput, 'index.html'))
 }
 
 init()
