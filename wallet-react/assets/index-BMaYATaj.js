2646403019ce505b484d5ad3ff12249aff6c7ede
chenweigh <chenweigh@users.noreply.github.com>
2024-09-11 09:38:53 +0000

    auto deploy example 4245ce9afa65d76553da52740a218e554a591197

---
+++ wallet-react/assets/index-BMaYATaj.js
@@ -0,0 +1,1 @@
+import{r as a,aF as f}from"./index-DJbFNRqX.js";import{u as c}from"./index-DEA0gw02.js";var l=function(n,r,e){e===void 0&&(e={});var u=c(n),t=a.useRef(null),i=a.useCallback(function(){t.current&&clearInterval(t.current)},[]);return a.useEffect(function(){if(!(!f(r)||r<0))return e.immediate&&u(),t.current=setInterval(u,r),i},[r,e.immediate]),i};export{l as u};
