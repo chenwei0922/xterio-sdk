1ca72917f8b2f8a738e0abe0823a001ca58b2d5f
ChenWei <chen158_7@126.com>
2024-09-11 11:22:41 +0800

    fix: merge conflict

--- sh/deploy.ts
+++ sh/deploy.ts
@@ -1,32 +1,52 @@
-import chalk from 'chalk'
-import consola from 'consola'
-import { copy, emptyDir, ensureDir } from 'fs-extra'
+import { copy, emptyDir } from 'fs-extra'
 import {
-  pathAuth,
-  pathWallet,
   pathExampleAuthReact,
   pathExample,
   pathExampleOutput,
-  pathExampleOutputAuthReact
+  pathExampleOutputAuthReact,
+  pathExampleAuthTG,
+  pathExampleOutputAuthTg,
+  pathExampleWalletReact,
+  pathExampleOutputWalletReact,
+  pathRoot
 } from './paths'
 import { run } from './run'
-import { dirname, resolve, join } from 'path'
+import { resolve, join } from 'path'
+import consola from 'consola'
+import chalk from 'chalk'
 
 async function init() {
   //example-auth-react
-  const srcdist = resolve(pathExampleAuthReact, 'dist')
-  const outDist = pathExampleOutputAuthReact
-  await emptyDir(srcdist)
-  await emptyDir(outDist)
-  await run('npm run build', pathExampleAuthReact)
-  await copy(srcdist, outDist)
+  // await buildExample(pathExampleAuthReact, pathExampleOutputAuthReact)
+  consola.log(chalk.greenBright('🎈example-auth-react build finish ✅'))
 
-  //example-auth-vue
+  //tg-mini-app
+  // await buildExample(pathExampleAuthTG, pathExampleOutputAuthTg, 'tg')
+  consola.log(chalk.green('🎈tg-mini-app build finish ✅'))
 
   //example-wallet-react
+  await buildExample(pathExampleWalletReact, pathExampleOutputWalletReact)
+  consola.log(chalk.green('🎈example-wallet-react build finish ✅'))
 
   //index.html
   await copy(join(pathExample, 'index.html'), join(pathExampleOutput, 'index.html'))
+
+  consola.log(chalk.green('🎈all examples build finish ✅'))
 }
 
+async function buildExample(input: string, output: string, _flag?: string) {
+  const srcdist = resolve(input, 'dist')
+  const outDist = output
+  await emptyDir(srcdist)
+  await emptyDir(outDist)
+
+  await run('pnpm run build', input)
+
+  // if (_flag === 'tg') {
+  //   await run('pnpm run -w build:tg-mini-app', pathRoot)
+  // } else {
+  //   await run('pnpm run build', input)
+  // }
+  await copy(srcdist, outDist)
+}
 init()
