097d8ca789d67f438392b89a10dca3724d186802
chenweigh <chenweigh@users.noreply.github.com>
2024-09-30 04:25:55 +0000

    auto deploy example 94fa67bbd74296760a4e90e68f4d9042ab4a8afb

---
+++ wallet-react/assets/masterPasswordDescription-MODK4JVE-BucMd_NL.js
@@ -0,0 +1,22 @@
+import{w as c}from"./chunk-IBLFF4W2-CGk3VBiw.js";import{p as n}from"./chunk-3WXPHVZ4-D--FIW4O.js";import{N as s}from"./chunk-6X7TBCV3-DzhoHidx.js";import{J as m,o as e}from"./index-BqQtutl2.js";import{u as l}from"./useTranslation-DvYF1WSU.js";import{C as i,T as p}from"./index-DyNgDjrE.js";import"./TextArea-DzsvBAoD.js";import"./colors-3gJVNNqF.js";import"./index-DWz4Eg6X.js";var u=`.master-password-description-container {
+  display: flex;
+  flex-direction: column;
+}
+.about-mp-container {
+  padding: 0 18px;
+}
+.about-mp-container .space-container {
+  width: 100%;
+  padding: 20px 0;
+}
+.about-mp-container .space-container .master-title {
+  text-align: center;
+}
+.about-mp-container .space-container .ant-typography {
+  color: var(--text-color) !important;
+}
+.about-mp-container .space-container .content {
+  font-size: 14px;
+  color: var(--secondary-text-color) !important;
+}
+`,d=()=>{let{Title:a,Paragraph:o}=p,r=m(),{t}=l();return e.createElement("div",{className:"master-password-description-container height-100"},e.createElement(c,null,t("account.about_master_password")),e.createElement("div",{className:"scroll-content"},e.createElement("style",null,u),e.createElement("div",{className:"about-mp-container"},e.createElement(n,{className:"icon-navigation-back",name:"circle_back",onClick:()=>r(-1)}),e.createElement(i,{direction:"vertical",className:"space-container"},e.createElement(a,{level:5,className:"title"},t("account.about_master_password_t1")),e.createElement(o,{className:"content"},t("account.about_master_password_p1")),e.createElement(a,{level:5,className:"title"},t("account.about_master_password_t2")),e.createElement(o,{className:"content"},t("account.about_master_password_p2")),e.createElement(a,{level:5,className:"title"},t("account.about_master_password_t3")),e.createElement(o,{className:"content"},t("account.about_master_password_p3"))))),e.createElement(s,{className:"footer-box-v2"}))},y=d;export{y as default};
---
+++ wallet-react/assets/paymentVerifyModal-CXP3DDCF-DXw_3rnD.js
@@ -0,0 +1,35 @@
+import{Q as f}from"./chunk-6FLCJIFF-C5NF8ku2.js";import{L as b,r as m,l as g,o as a,ax as h,E as c,ar as E}from"./index-BqQtutl2.js";import{u as x}from"./useRequest-CCk7zqdb.js";import{u as C}from"./useTranslation-DvYF1WSU.js";import"./chunk-3WXPHVZ4-D--FIW4O.js";import"./chunk-6X7TBCV3-DzhoHidx.js";import"./index-CYqhY_eO.js";import"./debounce-zdRIKzcF.js";import"./isObject-CrIk3fyR.js";import"./throttle-DGD7vI20.js";import"./index-ChVzBSY2.js";var P=`.payment-password-drawer {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.payment-password-drawer .ant-drawer-body {
+  padding-right: 0;
+  padding-left: 0;
+  overflow: hidden;
+}
+.payment-password-drawer .ant-drawer-content-wrapper {
+  width: 100% !important;
+}
+.payment-password-drawer .ant-drawer-content .ant-drawer-wrapper-body .ant-drawer-header {
+  display: none;
+}
+.payment-password-drawer .particle-pc-drawer .payment-verify-content {
+  margin-top: 38px;
+}
+.payment-password-drawer .payment-verify-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  margin-top: 22px;
+}
+.payment-password-drawer .payment-verify-content .particle-keywords-map {
+  position: absolute;
+}
+@media (min-width: 600px) {
+  .payment-password-drawer {
+    position: absolute;
+  }
+}
+`,_=({props:r})=>{let{setPaymentVerify:n,setWrongPassword:s}=b(),{t}=C(),[i,o]=m.useState(""),{authCoreModal:w}=g();m.useEffect(()=>{r.visible&&o("")},[r.visible]);let{loading:l,run:y}=x(E,{manual:!0,onSuccess:(e,p)=>{var d;n({visible:!1}),(d=r.onVerifyCompleted)==null||d.call(r,i)},onError:e=>{if(o(""),(e==null?void 0:e.error_code)===c.WrongPaymentPassword)s({visible:!0});else if((e==null?void 0:e.error_code)===c.SecurityAccountFrozen){let p=e.extra.seconds||0;s({visible:!0,accountFrozen:{seconds:p}})}}}),u=()=>{var e;if(l)return!1;n({visible:!1}),(e=r.onVerifyFailed)==null||e.call(r,t("common.cancel"))},v=e=>{o(e),e.length===6&&y(e)};return a.createElement(a.Fragment,null,a.createElement("style",null,P),a.createElement(h,{visible:r.visible,placement:"bottom",height:421,closable:!1,maskClosable:!1,onClose:u,className:"payment-password-drawer",title:r.type==="close"?t("account.close_payment_password"):t("account.payment_password"),forceRender:!0,getContainer:()=>w.rootBody},a.createElement("div",{className:"content payment-verify-content"},a.createElement(f,{onChange:v,value:i,keyboardInvisible:l}))))},B=_;export{B as default};
