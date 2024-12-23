4ee8fce7631f6c2a1802a6024253c2705cf4e461
henry <henry@playvrs.dev>
2024-12-23 14:29:39 +0800

    feat: update react-webgl template & example,optimize env variables config

--- examples/example-react-webgl-starter/src/config/index.ts
+++ examples/example-react-webgl-starter/src/config/index.ts
@@ -1,2 +1,1 @@
-export * from './chain-config'
 export * from './unity-config'
--- xterio-react-webgl-cli/template-xterio-react-webgl/src/main.tsx
+++ xterio-react-webgl-cli/template-xterio-react-webgl/src/main.tsx
@@ -1,64 +1,22 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import * as XterioAuth from '@xterio-sdk/auth'
 import '@xterio-sdk/auth/dist/es/style.css'
 import { addViewportMeta, validateConfig } from './utils/index.ts'
-import {
-  IXterioWalletContextProps,
-  SendTransactionMode,
-  XterioWalletProvider
-} from '@xterio-sdk/wallet'
+import { XterioWalletProvider } from '@xterio-sdk/wallet'
 
 import '@xterio-sdk/wallet/style/main.css'
 import { GlobalUnityProvider } from './contexts/GlobalUnityContext.tsx'
+import { getXterConfig } from './config/xterio-sdk-config.ts'
 
 // Add viewport meta to support mobile view
 addViewportMeta()
 
-const config: IXterioWalletContextProps = {
-  /**
-   * 申请平台账号接入时平台分配的app_id
-   */
-  app_id: '55f9d090bdb6', // demo app_id:55f9d090bdb6
-  /**
-   * 申请平台账号接入时平台分配的client_id
-   */
-  client_id: '1833749239520857', // demo client_id:1833749239520857
-  /**
-   * 申请平台账号接入时平台分配的client_secret
-   */
-  client_secret: 'ZGh5Nf4JcWp6jg3qMruYE9ye8xkFHtn2', // demo client_secret: ZGh5Nf4JcWp6jg3qMruYE9ye8xkFHtn2
-  /**
-   * 申请 particle newtork 钱包分配的 app id
-   */
-  pn_app_id: 'e45ec292-c9ec-4092-bdad-4435a2296c79', // demo pn_app_id: e45ec292-c9ec-4092-bdad-4435a2296c79
-  /**
-   * 申请平台账号接入时提交的redirect_uri,用于 sso 登录后跳转的地址，如果不是sso 登录可以忽略改参数
-   */
-  redirect_uri: '/',
-  /**
-   * 对接平台的环境类型： Dev | Staging | Production
-   */
-  env: XterioAuth.Env.Production,
-  /**
-   * 是否显示打开钱包弹窗悬浮按钮，默认为false
-   */
-  showOpenWalletIcon: false,
-  /**
-   * 是否在钱包sdk中自动集成auth模块并初始化，默认为true
-   */
-  enableAuthInit: true,
-  /**
-   * AA 钱包交易模式，默认为Gasless，UserSelect = 0 | Gasless = 1 | UserPaidNative = 2
-   */
-  transactionMode: SendTransactionMode.Gasless
-}
-
+const xterConfig = getXterConfig()
 // Check Xter SDK initial config
-validateConfig(config)
+validateConfig(xterConfig)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <XterioWalletProvider {...config}>
+  <XterioWalletProvider {...xterConfig}>
     <GlobalUnityProvider>
       <App />
     </GlobalUnityProvider>
